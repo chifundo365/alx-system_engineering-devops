6a0ae7230877ac70ae05208214179a16e16e906a
= <=>
2024-04-24 02:34:08 -0700

    Create a file

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# creates a file called 'school' in /tmp with owner, permission,group options
+file { '/tmp/school':
+  ensure  => file,
+  path    => '/tmp/school',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
+}
