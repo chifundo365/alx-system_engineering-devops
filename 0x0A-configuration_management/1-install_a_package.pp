6ba02b1d4823d53bc52f8fe59a46980e844f6d24
= <=>
2024-04-24 03:49:23 -0700

    Install a Packgage *

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,12 +1,13 @@
-# installs flask from pip3, version 2.1.0
+# Install an especific version of flask (2.1.0)
+
 exec { 'install_flask':
-  command => '/usr/bin/env pip3 install Flask==2.0.1',
+  command => '/usr/bin/pip3 install flask==2.1.0',
   path    => ['/usr/bin'],
-  unless  => '/usr/bin/env pip3 show flask | grep -q "Version: 2.1.0"',
+  unless  => '/usr/bin/pip3 show flask | grep -q "Version: 2.1.0"',
 }
 
-exec {'install_werkzeug':
-  command => '/usr/bin/env pip3 install werkzeug==2.1.1',
+exec { 'install_werkzeug':
+  command => '/usr/bin/pip3 install werkzeug==2.1.1',
   path    => ['/usr/bin'],
-  unless  => '/usr/bin/env pip3 show werkzeug | grep -q "Version: 2.1.1"',
+  unless  => '/usr/bin/pip3 show werkzeug | grep -q "Version: 2.1.1"',
 }
