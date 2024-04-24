f825b30264564ee1c51100197b9dc9f06e2170dd
= <=>
2024-04-24 03:31:55 -0700

    Install a package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,6 @@
 # installs flask from pip3, version 2.1.0
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
+exec { 'install_flask':
+  command => '/usr/bin/env pip3 install Flask==2.0.1',
+  path    => ['/usr/bin'],
+  unless  => '/usr/bin/env pip3 show flask | grep -q "Version: 2.1.0"',
 }
