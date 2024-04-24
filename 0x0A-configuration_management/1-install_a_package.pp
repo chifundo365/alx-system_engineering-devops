c0f4708bc1888ce3e57db117ae0e366d54296b3b
= <=>
2024-04-24 03:40:59 -0700

    Install a package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -4,3 +4,9 @@ exec { 'install_flask':
   path    => ['/usr/bin'],
   unless  => '/usr/bin/env pip3 show flask | grep -q "Version: 2.1.0"',
 }
+
+exec {'install_werkzeug':
+  command => '/usr/bin/env pip3 install werkzeug==2.1.1'
+  path    => ['/usr/bin'],
+  unless  => '/usr/bin/env pip3 show werkzeug | grep -q "Version: 2.1.1"'
+}
