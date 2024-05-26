2c43275176ef0ee741ddd8fa2547e69b57548798
= <=>
2024-05-26 07:41:35 -0700

    Export to CSV

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+export data got from a request using an API in csv format
+"""
+import csv
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com/"
+    username = requests.get(url+"users/{}".format(argv[1])).json().get(
+            "username"
+            )
+    tasks = requests.get(url+"todos", params={"userId": argv[1]}).json()
+    data = []
+    for task in tasks:
+        data.append([argv[1],
+                    username, task.get("completed"),
+                    task.get('title')])
+    filename = argv[1]+".csv"
+    with open(filename, "w", newline="") as file:
+        csvwriter = csv.writer(file)
+        csvwriter.writerows(data)
