c28d2f51062b1ac1f41e1ec8b3489003d88db08d
= <=>
2024-06-07 02:41:28 -0700

    How many subs

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Queries the RedditApi and processes its data
+"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """
+    Returns number of subscribers in a given subreddit
+    """
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"User-Agent": "My-User-Agent"}
+    sub_r_subscribers = requests.get(url, headers=headers, allow_redirects=False)
+    if sub_r_subscribers.status_code >= 300:
+        return 0
+    return sub_r_subscribers.json().get("data").get("subscribers")
---
+++ 0x16-api_advanced/README.md
@@ -0,0 +1,3 @@
+#0x16-api_advanced
+working with the reddit api
+learning how authentication with an api works
